ccff794597ec0c342a533f7d3a81324143cbae4c
IsaacLusca <90662483+IsaacLusca@users.noreply.github.com>
2021-10-01 10:34:52 -0300

    Update Isaac_linhaAtualizado.py

--- Isaac_linhaAtualizado.py
+++ Isaac_linhaAtualizado.py
@@ -6,7 +6,7 @@ df = pd.read_csv('annual-co2-emissions-per-country(2).csv', sep=',', usecols=['E
 #Transformando a lista em array
 df_array = df.values
 
-#criando listas vazias
+#criando listas vazias para manipular depois
 pais = []
 code = []
 ano = []
@@ -19,13 +19,20 @@ for i in df_array:
   ano.append(i[2])
   CO2_emitido.append(i[3])
 
-#Criação e edição do gráfico
-imagem = go.Figure(go.Scatter(x=ano,
+#Criação e edição do gráfico / Eixo x
+#Definindo qual valor será representado no eixo Y.
+#mode define o tipo de linha que o gráfico terá. 
+#text define o padrão que as linhas armazenará, e nesse caso será o nome dos países.
+imagem = go.Figure(go.Scatter(x=ano,                         
                               y=CO2_emitido,
                               mode='lines',
                               text=pais))
 
 #Acrescentando algumas características ao gráfico
+#Permite a manipulação das características do gráfico
+ #Title representa o "Nome principal" Do gráfico
+ #Template altera o tema de fundo do gráfico
+#Xaxis e Yaxis acrescentam, respectivamente, o nome do eixo x e o y do gráfico
 graf_linha = go.Figure(imagem)
 graf_linha.update_layout(
     title_text='Emissão de CO2: Alemanha, America do sul, Asia, Brasil, China, Europa, EUA',
@@ -35,7 +42,5 @@ graf_linha.update_layout(
     yaxis = dict(
         title = 'Concentração de CO2'
     )
-)
-    
-
+)  
 graf_linha.show()
