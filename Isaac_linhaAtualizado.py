439cb6efb80a1e707321eb54c1e2ed1452a2b8ac
IsaacLusca <90662483+IsaacLusca@users.noreply.github.com>
2021-10-01 13:08:12 -0300

    Update Isaac_linhaAtualizado.py

--- Isaac_linhaAtualizado.py
+++ Isaac_linhaAtualizado.py
@@ -1,35 +1,51 @@
 import plotly.graph_objects as go
 import pandas as pd
 
-df = pd.read_csv('annual-co2-emissions-per-country(2).csv', sep=',', usecols=['Entity','Code', 'Year', 'Annual CO2 emissions'])
+df = pd.read_csv('/content/annual-co2-emissions-per-country(3) - annual-co2-emissions-per-country(3).csv', sep=',')
 
 #Transformando a lista em array
 df_array = df.values
 
 #criando listas vazias para manipular depois
-pais = []
-code = []
 ano = []
-CO2_emitido = []
+asia = []
+brasil = []
+china = []
+europa = []
+alemanha = []
+americsul = []
+usa = []
+world = []
+
 
 #Extrutura de repetição:
 for i in df_array:
-  pais.append(i[0])
-  code.append(i[1])
-  ano.append(i[2])
-  CO2_emitido.append(i[3])
-
+  ano.append(i[0])
+  asia.append(i[1])
+  brasil.append(i[2])
+  china.append(i[3])
+  europa.append(i[4])
+  alemanha.append(i[5])
+  americsul.append(i[6])
+  usa.append(i[7])
+  world.append(i[8])
 #Criação e edição do gráfico / Eixo x
 #Definindo qual valor será representado no eixo Y.
 #mode define o tipo de linha que o gráfico terá. 
-#text define o padrão que as linhas armazenará, e nesse caso será o nome dos países.
-imagem = go.Figure(go.Scatter(x=ano,                         
-                              y=CO2_emitido,
-                              mode='lines',
-                              text=pais))
+#name define o padrão que as linhas armazenará, e nesse caso será o nome de cada país.
+linha1 = go.Scatter(x=ano, y= asia, mode = 'lines', name = 'Asia')
+linha2 = go.Scatter(x = ano, y = brasil, mode = 'lines', name = 'Brasil')
+linha3 = go.Scatter(x = ano, y = china, mode = 'lines', name = 'China')
+linha4 = go.Scatter(x = ano, y = europa, mode = 'lines', name = 'Europe')
+linha5 = go.Scatter(x=ano, y= alemanha, mode = 'lines', name = 'Germany')
+linha6 = go.Scatter(x = ano, y = americsul, mode = 'lines', name = 'South America')
+linha7 = go.Scatter(x = ano, y = usa, mode = 'lines', name = 'United States')
+linha8 = go.Scatter(x = ano, y = world, mode = 'lines', name = 'World')
 
-#Comando que permite a manipulação das características do gráfico
- 
+#Definindo as listas como imagem para facilitar na manipulação
+imagem = [linha1, linha2, linha3, linha4, linha5, linha6, linha7, linha8]
+
+#Comando que permite a manipulação das características do gráfico 
 graf_linha = go.Figure(imagem)
 graf_linha.update_layout(
     title_text='Emissão de CO2: Alemanha, America do sul, Asia, Brasil, China, Europa, EUA',
